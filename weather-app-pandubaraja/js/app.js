9c105c86ddfe1f685fe4ab97f8e7bed3c93d6a76
Pandu Baraja <pandu.baraja@gmail.com>
2024-08-13 20:06:11 +0700

    [Feature] Weather App (#6)
    * handle weather app
    
    * Add default state and use config to get key
    
    * Add key

---
+++ weather-app-pandubaraja/js/app.js
@@ -0,0 +1,172 @@
+import { fetchWeather, fetchWeatherLatLong } from './weatherApi.js';
+
+class WeatherApp {
+    constructor() {
+        this.searchBtn = document.getElementById('search-btn');
+        this.locationBtn = document.getElementById('location-btn')
+        this.cityInput = document.getElementById('city-input');
+        this.weatherContainer = document.getElementById('weather-container');
+        this.historyList = document.getElementById('history-list');
+
+        const localHistories = localStorage.getItem('search_histories')
+        this.searchHistory = localHistories ? JSON.parse(localHistories) : [];
+
+        this.init();
+    }
+
+    init() {
+        this.searchBtn.addEventListener('click', () => this.handleSearch())
+        this.locationBtn.addEventListener('click', () => this.handleLocation())
+        this.cityInput.addEventListener('keypress', (event) => {
+            if(event.key == 'Enter') this.handleSearch()
+        })
+        this.historyList.addEventListener('click', (event) => this.handleHistoryClick(event))
+
+        const urlParams = new URLSearchParams(window.location.search)
+        const cityParam = urlParams.get('city')
+        if (cityParam) {
+            this.cityInput.value = cityParam
+            this.handleSearch()
+        }
+
+        this.updateHistoryList()
+    }
+
+    handleSearch() {
+        const city = this.cityInput.value.trim()
+
+        if (city) {
+            this.displayLoading()
+
+            fetchWeather(city)
+                .then(data => {
+                    this.handleState(data)
+                })
+        }
+    }
+
+    handleState(state) {
+        switch (state.state) {
+            case 'success': 
+                this.displayWeather(state.data)
+                this.cityInput.value = state.data.city
+                this.addToHistory(state.data.city)
+                break;
+            case 'empty':
+                this.displayEmpty()
+                break;
+            case 'error': 
+                this.displayError(state.message)
+                break;
+            default: break
+        }
+    }
+
+    handleLocation() {
+        this.displayLoading()
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const lat = position.coords.latitude;
+                    const lon = position.coords.longitude;
+
+                    fetchWeatherLatLong(lat, lon)
+                        .then(state => {
+                            this.handleState(state)
+                            this.updateURL(state.data.city)
+                        })
+                },
+                (error) => {
+                    switch(error.code) {
+                        case error.PERMISSION_DENIED:
+                            this.displayError("User denied the request for Geolocation.");
+                            break;
+                        case error.POSITION_UNAVAILABLE:
+                            this.displayError("Location information is unavailable.");
+                            break;
+                        case error.TIMEOUT:
+                            this.displayError("The request to get user location timed out.");
+                            break;
+                        case error.UNKNOWN_ERROR:
+                            this.displayError("An unknown error occurred.");
+                            break;
+                    }
+                },
+                {
+                    timeout: 5000
+                }
+            );
+        } else {
+            this.displayError("Geolocation is not supported by this browser.");
+        }
+    }
+
+    displayWeather(data) {
+        const weatherHTML = `
+                <dotlottie-player src="${data.weatherLottie}" background="transparent" speed="1" style="width: 300px; height: 300px;" loop autoplay></dotlottie-player>
+                <div id='weather-details'>
+                    <span class='title-lg'>${data.temperature}º</span>
+                    <span class='title-md'>${data.weather}</span>
+                    <span class='title-sm'>${data.city}, ${data.country}</span>
+                    <ul class='horizontal-info'>
+                        <li><i class="fa-solid fa-droplet"></i>${data.humidity}</li>
+                        <li><i class="fa-solid fa-wind fa-flip-horizontal"></i>${data.windSpeed}</li>
+                        <li><i class="fa-solid fa-water"></i>${data.seaLevel}</li>
+                    </ul>
+                </div>
+        `
+        
+        this.weatherContainer.innerHTML = weatherHTML
+    }
+
+    displayLoading() {
+        this.weatherContainer.innerHTML = `<div class='loader'></div>`
+    }
+
+    displayEmpty() {
+        this.weatherContainer.innerHTML = '<p>City not found. Please try again.</p>'
+    }
+    
+    displayError(error) {
+        this.weatherContainer.innerHTML = `<p>Error: ${error}</p>`
+    }
+
+    addToHistory(city) {
+        if (this.searchHistory.includes(city)) {
+            const index = this.searchHistory.indexOf(city)
+            this.searchHistory.splice(index, 1);
+            this.searchHistory.unshift(city);
+            this.updateHistoryList()
+            return
+        }
+        
+        this.searchHistory.unshift(city)
+
+        if (this.searchHistory.length > 5) this.searchHistory.pop()
+
+        localStorage.setItem('search_histories', JSON.stringify(this.searchHistory))
+        
+        this.updateHistoryList()
+        this.updateURL(city)
+    }
+
+    updateHistoryList() {
+        this.historyList.innerHTML = this.searchHistory.map(city => `<li>${city}</li>`).join("")
+    }
+
+    handleHistoryClick(e) {
+        if (e.target.tagName === 'LI') {
+            this.cityInput.value = e.target.textContent
+            this.handleSearch()
+            this.updateURL(e.target.textContent)
+        }
+    }
+
+    updateURL(city) {
+        const url = new URL(window.location)
+        url.searchParams.set('city', city)
+        window.history.pushState({}, '', url)
+    }
+}
+
+new WeatherApp();
---
+++ weather-app-pandubaraja/js/weatherApi.js
@@ -0,0 +1,98 @@
+const WEATHER_API_KEY = 'bb2cf1d8174eb033fe6324cba567dd92'
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const WeatherType = Object.freeze ({
+    THUNDERSTORM: 'Thunderstorm',
+    RAIN: 'Rain',
+    DRIZZLE: 'Drizzle',
+    SNOW: 'Snow',
+    CLEAR: 'Clear',
+    CLOUDS: 'Clouds'
+})
+
+export class Weather {
+    
+    constructor(data) {
+        const weather = data.weather[0]
+
+        this.city = data.name
+        this.country = data.sys.country
+        this.temperature = Math.round(data.main.temp - 273.15)
+        this.weather = this.capitalizeWords(weather.description)
+        this.humidity = data.main.humidity
+        this.windSpeed = data.wind.speed
+        this.seaLevel = data.main.sea_level
+        this.weatherLottie = this.getLottie(weather.main)
+        this.weatherIcon = `https://openweathermap.org/img/wn/${weather.icon}@2x.png`
+    }
+
+    getLottie(weather) {
+        switch (weather) {
+            case WeatherType.THUNDERSTORM:
+                return "https://lottie.host/c04be981-6c2e-4150-b9bd-22e6065da9ee/774AcxrUQg.json"
+            case WeatherType.RAIN:
+                return "https://lottie.host/f2acfd89-c93f-4f60-84f2-23919c7af2e6/PDWq1Q97RJ.json"
+            case WeatherType.DRIZZLE:
+                return "https://lottie.host/f2acfd89-c93f-4f60-84f2-23919c7af2e6/PDWq1Q97RJ.json"
+            case WeatherType.SNOW:
+                return "https://lottie.host/c9eced40-3d2f-4ded-b004-a79ceacc76d1/vUowqhEFtd.json"
+            case WeatherType.CLEAR:
+                return "https://lottie.host/af67714c-f912-40a9-9ee4-c1ab8ce737be/ddtjRUuacf.json"
+            case WeatherType.CLOUDS:
+                return "https://lottie.host/4273c15b-9e85-4ed7-8b22-bccfae4b3a77/hgrFpWNPUy.json"
+            default: return "https://lottie.host/4d25adc5-cc0b-44b4-a5ef-e413782dbe3f/mbiJVPGOQe.json"
+        }
+    }
+
+    capitalizeWords(string) {
+        return string.split(' ').map(word => {
+            if (!word) return ''; 
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        }).join(' ');
+    }
+}
+
+export function fetchWeather(city) {
+  return fetch(buildUrl(BASE_URL, {q: city}))
+    .then(handleWeatherResponse)
+    .catch(handleError);
+}
+
+export function fetchWeatherLatLong(lat, lon) {
+    return fetch(buildUrl(BASE_URL, {
+        lat: lat,
+        lon: lon
+      }))
+      .then(handleWeatherResponse)
+      .catch(handleError);
+}
+
+function buildUrl(baseUrl, params = {}) {
+    const url = new URL(baseUrl);
+
+    url.searchParams.append('appId', WEATHER_API_KEY)
+    
+    Object.keys(params).forEach(key => {
+        url.searchParams.append(key, params[key]);
+    });
+    
+    return url.toString();
+}
+
+function handleWeatherResponse(response) {
+    if (!response.ok) {
+        return { state: 'empty', message: 'Weather data not found' }
+    }
+
+    return response.json()
+        .then(data => {
+          return {
+              state: 'success',
+              data: new Weather(data)
+          }
+      })
+}
+
+function handleError(error) {
+    return { state: 'error', code: error.status, message: error.message };
+}
