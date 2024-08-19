525ae0647a64e920090bb4b27136c6c6c7e7dc2f
pandubaraja <pandu.baraja@gmail.com>
2024-08-19 18:49:19 +0700

    handle histories and strength indicator

--- password-generator-app-pandubaraja/js/app.js
+++ password-generator-app-pandubaraja/js/app.js
@@ -12,6 +12,12 @@ class App {
         this.passwordEl = document.getElementById('password')
         this.lengthEl = document.getElementById('length')
         this.urlEl = document.getElementById('url')
+        this.uppercaseCheck = document.getElementById('uppercase')
+        this.lowercaseCheck = document.getElementById('lowercase')
+        this.numberCheck = document.getElementById('number')
+        this.symbolCheck = document.getElementById('symbol')
+        this.strength = document.getElementById('strength')
+        this.histories = document.getElementById('histories')
 
         this.init()
     }
@@ -22,25 +28,32 @@ class App {
         })
 
         this.copyBtn.addEventListener('click', (e) => {
-            navigator.clipboard.writeText(this.passwordEl.value)
-                .then(() => {
-                    alert('Password copied to clipboard')
-                })
-                .catch((error) => {
-                    alert("Failed to copy text: ", error)
-                })
+            this.copyToClipboard(this.passwordEl.value)
         })
+
+        this.renderPasswordHistories()
     }
 
     generatePassword = () => {
         const length = parseInt(this.lengthEl.value)
-        const result = this.passwordGenerator.generatePassword(length)
+
+        const result = this.passwordGenerator.generatePassword(
+            length,
+            {
+                uppercase: this.uppercaseCheck.checked,
+                lowercase: this.lowercaseCheck.checked,
+                numbers: this.numberCheck.checked,
+                symbols: this.symbolCheck.checked
+            }
+        )
 
         switch (result.status) {
             case 'success':
                 const url = this.urlEl.value
-                const password = result.data
+                const password = result.data.password
                 this.passwordEl.value = password
+                this.strength.textContent = result.data.strength
+                this.strength.setAttribute('style', `color: ${result.data.color}; font-weight: bold;`) 
 
                 this.savePassword({
                     password,
@@ -48,7 +61,7 @@ class App {
                 })
                 break;
             case 'error':
-                console.log(error)
+                alert(result.message)
                 break;
             default: break;
         }
@@ -63,6 +76,44 @@ class App {
         if (newPasswordData.length > 5) newPasswordData.pop()
     
         this.storage.save('passwords', newPasswordData)
+
+        this.renderPasswordHistories()
+    }
+
+    renderPasswordHistories = () => {
+        this.histories.innerHTML = ''
+        const stored = this.storage.get('passwords')
+
+        if (stored) {
+            stored.forEach((element, index) => {
+                const row = document.createElement('div')
+                row.classList.add('history-item')
+                row.innerHTML = `
+                    <div>${index + 1}</div>
+                    <div>${element.url ? element.url : 'none'}</div>
+                    <div>${element.password}</div>
+                `
+    
+                row.addEventListener('click', (e) => {
+                    e.stopPropagation()
+                    this.copyToClipboard(element.password)
+                })
+                this.histories.appendChild(row)
+            });
+        }
+        else {
+            this.histories.innerHTML = '<p>No password histories</p>'
+        }   
+    }
+
+    copyToClipboard = (data) => {
+        navigator.clipboard.writeText(data)
+            .then(() => {
+                alert('Password copied to clipboard')
+            })
+            .catch((error) => {
+                alert("Failed to copy text: ", error)
+            })
     }
 }
 
